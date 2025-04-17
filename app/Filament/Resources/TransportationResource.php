4f38225ac71b4f084d885c7e388a862a8ba5f4ff
fkeen <keenvergara@gmail.com>
2025-04-17 15:18:43 +0800

    feat(transportations): add transportations crud

---
+++ app/Filament/Resources/TransportationResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TransportationResource\Pages;
+use App\Models\Transportation;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Forms;
+
+class TransportationResource extends Resource
+{
+    protected static ?string $model = Transportation::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('email')->required()->email(),
+                Forms\Components\TextInput::make('phone')->required(),
+                Forms\Components\TextInput::make('website')->nullable(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('phone'),
+                Tables\Columns\TextColumn::make('website'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTransportations::route('/'),
+            'create' => Pages\CreateTransportation::route('/create'),
+            'edit' => Pages\EditTransportation::route('/{record}/edit'),
+        ];
+    }
+}
