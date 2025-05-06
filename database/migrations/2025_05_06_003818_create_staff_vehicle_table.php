6c0b69e8d773baba22b2061ba9cd224e8811ba82
fkeen <keenvergara@gmail.com>
2025-05-06 08:59:11 +0800

    feat: add migrations for staff-vehicle listing and bookings

---
+++ database/migrations/2025_05_06_003818_create_staff_vehicle_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('staff_vehicle', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('staffable');
+            $table->foreignId('vehicle_id')->constrained('vehicles')->cascadeOnDelete();
+            $table->dateTime('starts_at');
+            $table->dateTime('ends_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('staff_vehicle');
+    }
+};
---
+++ database/migrations/2025_05_06_003819_create_bookings_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('staffable');
+            $table->foreignId('staff_id')->constrained('staffs')->cascadeOnDelete();
+            $table->foreignId('guest_id')->constrained('guests')->cascadeOnDelete();
+            $table->foreignId('vehicle_id')->constrained('vehicles')->cascadeOnDelete();
+            $table->unsignedInteger('passengers');
+            $table->string('description', 100)->nullable();
+            $table->dateTime('pickup_at');
+            $table->dateTime('dropped_off_at');
+            $table->dateTime('actual_pickup_at')->nullable();
+            $table->dateTime('actual_dropped_off_at')->nullable();
+            $table->enum('status', ['pending', 'confirmed', 'for pick up', 'in transit', 'dropped off', 'cancelled', 'invalid']);
+            $table->timestamps();
+            
+            $table->unique(['staffable_type', 'staffable_id', 'staff_id', 'guest_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('staff_vehicle');
+    }
+};
